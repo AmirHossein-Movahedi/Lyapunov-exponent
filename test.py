2b091ba17d13af9ab2b202ecd534d8a12ba2cf27
AmirHosseinMovahedi <112428778+AmirHossein-Movahedi@users.noreply.github.com>
2025-10-28 16:52:25 +0330

    Update test.py

--- test.py
+++ test.py
@@ -3,12 +3,11 @@ import numpy as np
 import pandas as pd 
 
 initial_conditions = np.random.rand(3)
-
 file = np.array(pd.read_csv("UPO.csv").iloc[:,1:])
 df_array = np.array(file)
 
 
-L = Lyapunov(start_time=0.0001, end_time=50, dt=0.001,initial_condition=initial_conditions)
+L = Lyapunov(start_time=0.0001, end_time=200, dt=0.01,initial_condition=initial_conditions)
 result = L.output_hints_method(file)
 # print(result["t"])
 # print("\n -------------------------- \n")
